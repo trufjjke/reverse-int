a4cb3712cdab0c2cbd7972473efb454eddc0c7de
Vladislav <trufanov.vl2013@gmail.com>
2023-01-10 20:47:11 +0300

    feat: test complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+   if (n < 0) {
+      n = n * -1;
+   }
+   let arr = [];
+   let result = ``;
+   let strArg = n.toString();
+   for (let i = 0; i < strArg.length; i++) {
+      arr[i] = strArg[i];
+   }
+   result = arr.reverse().join(``);
+   return result;
+};
+console.log(module.exports(-1));
